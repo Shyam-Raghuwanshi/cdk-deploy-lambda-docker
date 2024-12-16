418c399151fea3a4d674f53bb2e3b4c0933d35dd
Shyam-Raghuwanshi <sr9993663832@gmail.com>
2024-12-16 22:18:28 +0530

    adding-api-endpoints

--- lib/cdk-deploy-lambda-docker-stack.ts
+++ lib/cdk-deploy-lambda-docker-stack.ts
@@ -1,12 +1,12 @@
-import { CfnOutput, Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import cdk = require("aws-cdk-lib");
 import {
   Architecture,
   DockerImageCode,
   DockerImageFunction,
-  FunctionUrlAuthType,
 } from "aws-cdk-lib/aws-lambda";
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
 export class CdkDeployLambdaDockerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -14,11 +14,11 @@ export class CdkDeployLambdaDockerStack extends Stack {
     // Define the Docker image Lambda function
     const lambdaFunction = new DockerImageFunction(
       this,
-      "lambdaFunction2",
+      "lambdaFunction5",
       {
         code: DockerImageCode.fromImageAsset("lib/docker"),
         architecture: Architecture.X86_64,
-        memorySize: 512,
+        memorySize: 1024,
         timeout: cdk.Duration.seconds(300),
         environment: {
           NODE_OPTIONS: "--enable-source-maps",
@@ -26,12 +26,16 @@ export class CdkDeployLambdaDockerStack extends Stack {
       }
     );
     // Add a Function URL
-    const functionUrl = lambdaFunction.addFunctionUrl({
-      authType: FunctionUrlAuthType.NONE,
-    });
+    // const functionUrl = lambdaFunction.addFunctionUrl({
+    //   authType: FunctionUrlAuthType.NONE,
+    // });
 
-    new CfnOutput(this, "FunctionUrl", {
-      value: functionUrl.url,
-    });
+    // new CfnOutput(this, "FunctionUrl", {
+    //   value: functionUrl.url,
+    // });
+
+    new apigw.LambdaRestApi(this, 'myapi', {
+      handler: lambdaFunction,
+    })
   }
 }
--- lib/docker/app.ts
+++ lib/docker/app.ts
@@ -1,33 +1,73 @@
 // @ts-nocheck
-import { generateEmbeddings, ModelTypes } from "@shyamraghuonec/rust-lib";
 
-export const handler = async (event) => {
-  console.log("Full Event:", JSON.stringify(event, null, 2));
-  try {
-    console.log("Attempting to scrape website");
-    const scrapeResult = await Promise.race([
-      await generateEmbeddings(["hi"], ModelTypes.AllMiniLML12V2),
-      new Promise((_, reject) =>
-        setTimeout(() => reject(new Error("Scrape timeout")), 2000)
-      ),
-    ]);
+// export const handler = async (event) => {
+//   console.log("Full Event:", JSON.stringify(event, null, 2));
+//   try {
+//     console.log("Attempting to scrape website");
+//     const scrapeResult = await Promise.race([
+//       await generateEmbeddings(["hi"], ModelTypes.AllMiniLML12V2),
+//       new Promise((_, reject) =>
+//         setTimeout(() => reject(new Error("Scrape timeout")), 2000)
+//       ),
+//     ]);
 
-    console.log("Scrape Result:", scrapeResult);
+//     console.log("Scrape Result:", scrapeResult);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ data: scrapeResult }),
-    };
-  } catch (error: any) {
-    console.error("Scraping Error:", {
-      errorName: error.name,
-      errorMessage: error.message,
-      errorStack: error.stack,
-    });
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ data: scrapeResult }),
+//     };
+//   } catch (error: any) {
+//     console.error("Scraping Error:", {
+//       errorName: error.name,
+//       errorMessage: error.message,
+//       errorStack: error.stack,
+//     });
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.toString() }),
-    };
-  }
-};
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: error.toString() }),
+//     };
+//   }
+// };
+
+import { Hono } from 'hono'
+import { generateEmbeddings, ModelTypes, scrapeWebsite, scrapeTextFromUrls, scrapeTextFromUrlPlaywright } from "@shyamraghuonec/rust-lib";
+import { handle } from 'hono/aws-lambda'
+const app = new Hono()
+
+app.post('/', async (c) => {
+  const body = await c.req.json();
+  return c.json({ data: body });
+});
+
+// app.get('/', (c) => {
+//   return c.json({ message: 'Hello World!' })
+// })
+
+
+app.post('/generateEmbeddings', async (c) => {
+  const { text } = await c.req.json();
+  const embeddings = await generateEmbeddings(text, ModelTypes.AllMiniLML12V2);
+  return c.json({ embeddings });
+})
+
+app.post('/scrapeTextFromUrlPlaywright', async (c) => {
+  const { url } = await c.req.json();
+  const text = await scrapeTextFromUrlPlaywright(url)
+  return c.text(JSON.stringify(text));
+});
+
+app.post("/scrapeWeb", async (c) => {
+  const { url } = await c.req.json();
+  const urls = await scrapeWebsite(url);
+  return c.json({ urls });
+});
+
+app.post("/scrapeTextFromUrls", async (c) => {
+  const { urls } = await c.req.json();
+  const textArr = await scrapeTextFromUrls(urls);
+  return c.text(JSON.stringify(textArr));
+});
+
+export const handler = handle(app)
